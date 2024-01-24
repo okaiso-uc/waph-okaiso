5ad30339663933cbd757e0871f7214175e7d214a
Sethoka1 <156627165+Sethoka1@users.noreply.github.com>
2024-01-23 19:41:03 -0500

    Update index.c

--- labs/lab1/index.c
+++ labs/lab1/index.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void) {
    printf("Content-Type :text/plain; charset=utf-8\n\n");
-   printf("<!DOCTYPE html> <html> <head> <title> Seth Okai </title> </head> <body> <h1> WAPH </h1> <p>Welcome</p></body>
-</html>\n\n");
+   printf("<!DOCTYPE html> <html> <head> <title> Seth Okai </title> </head> <body> <h1> WAPH </h1> <p>Welcome</p></body></html>\n\n");
   return 0;
 }
 
