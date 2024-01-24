f11459edc81345b9f746e4aea5f669b51a0d1684
Sethoka1 <156627165+Sethoka1@users.noreply.github.com>
2024-01-23 19:36:22 -0500

    Update index.c

--- labs/lab1/index.c
+++ labs/lab1/index.c
@@ -1,4 +1,7 @@
-<!DOCTYPE html>
+#include <stdio.h>
+int main(void) {
+   printf("Content-Type :text/plain; charset=utf-8\n\n");
+   printf("<!DOCTYPE html>
 <html>
 <head>
 <title>Welcome to My Website Title</title>
@@ -9,4 +12,7 @@
 <p>Thank you.</p>
 
 </body>
-</html>
+</html>\n\n");
+  return 0;
+}
+
