769d2e81fc208cab895797e67f602a6fa11802d3
Sethoka1 <156627165+Sethoka1@users.noreply.github.com>
2024-01-24 21:19:04 -0500

    Update index.c

--- labs/lab1/index.c
+++ labs/lab1/index.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+
 int main() {
-   printf("Content-Type: text/plain; charset=utf-8\n\n");
+    // Set the Content-Type header to text/html
+    printf("Content-Type: text/html; charset=utf-8\n\n");
 
-   printf("<html>\n");
-   printf("<head>\n");
-   printf("<title> WAPH</title>\n");
-   printf("</head>\n");
-   printf("<body>\n");
-   printf("<h1>Hi welcome to my site</h1>\n");
-   printf("</body>\n");
-   printf("</html>\n");
-   
-   return 0;
+    // Print the HTML content
+    printf("<html>\n");
+    printf("<head>\n");
+    printf("<title>WAPH</title>\n");
+    printf("</head>\n");
+    printf("<body>\n");
+    printf("<h1>Hi, welcome to my site</h1>\n");
+    printf("</body>\n");
+    printf("</html>\n");
 
-
+    return 0;
+}
