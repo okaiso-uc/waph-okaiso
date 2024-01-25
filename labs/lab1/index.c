b22dace181426333f047eda4a4bb373a04ad0880
Sethoka1 <156627165+Sethoka1@users.noreply.github.com>
2024-01-24 21:25:05 -0500

    Update index.c

--- labs/lab1/index.c
+++ labs/lab1/index.c
@@ -10,7 +10,7 @@ int main() {
     printf("<title>WAPH</title>\n");
     printf("</head>\n");
     printf("<body>\n");
-    printf("<h1>Hi, welcome to my site</h1>\n");
+    printf("<h1>Hi I am Seth Okai, welcome to my site</h1>\n");
     printf("</body>\n");
     printf("</html>\n");
 
