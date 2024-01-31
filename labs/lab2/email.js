954d6b6a09e65185f9309f6000a4e08c907aae81
Sethoka1 <156627165+Sethoka1@users.noreply.github.com>
2024-01-31 15:03:55 -0500

    Update email.js

--- labs/lab2/email.js
+++ labs/lab2/email.js
@@ -1,12 +1,17 @@
 var shown = false;
+
 function showhideEmail() {
-   if   (shown){
-      document.getElementById('email').innerHTML = "Show my email"; 
-      shown = false;
-  }else{ 
-    var myemail = "<a href='mailto:okaiso"+ "@" +
-                "ucmail.uc.edu'>okaiso" + "@" + "ucmail.uc.edu</a>
-    document.getElementById('email').innerHTML= myemail;
+  if (shown) {
+    document.getElementById('email').innerHTML = "Show my email";
+    shown = false;
+  } else {
+    var myemail =
+      "<a href='mailto:okaiso" +
+      "@" +
+      "ucmail.uc.edu'>okaiso" +
+      "@" +
+      "ucmail.uc.edu</a>";
+    document.getElementById('email').innerHTML = myemail;
     shown = true;
   }
 }
