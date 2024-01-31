fee13848418ee77f95776536cd2266a7f916707e
Sethoka1 <156627165+Sethoka1@users.noreply.github.com>
2024-01-31 14:47:03 -0500

    Create email.js

---
+++ labs/lab2/email.js
@@ -0,0 +1,12 @@
+var shown = false;
+function showhideEmail(){
+  if    (shown){
+      document.getElementById('email').innerHTML = "Show my email"; 
+      shown = false;
+  }else{ 
+    var myemail = "<a href='mailto:okaiso"+ "@" +
+                "ucmail.uc.edu'>okaiso" + "@" + "ucmail.uc.edu</a>
+    document.getElementById('email').innerHTML= myemail;
+    shown = true;
+  }
+}
