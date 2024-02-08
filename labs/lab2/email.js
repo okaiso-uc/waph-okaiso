f326584d20457d11a31e0e294326343692f5a496
Sethoka1 <156627165+Sethoka1@users.noreply.github.com>
2024-02-07 22:49:29 -0500

    XSS protection for email.js

--- labs/lab2/email.js
+++ labs/lab2/email.js
@@ -2,16 +2,29 @@ var shown = false;
 
 function showhideEmail() {
   if (shown) {
-    document.getElementById('email').innerHTML = "Show my email";
+    document.getElementById('email').textContent = "Show my email";
     shown = false;
   } else {
-    var myemail =
-      "<a href='mailto:okaiso" +
-      "@" +
-      "ucmail.uc.edu'>okaiso" +
-      "@" +
-      "ucmail.uc.edu</a>";
-    document.getElementById('email').innerHTML = myemail;
-    shown = true;
+    var input = document.getElementById('emailInput').value;
+    var encodedInput = encodeInput(input);
+    if (validateEmail(encodedInput)) {
+      var encodedEmail = encodeURI(encodedInput);
+      var myemail = "<a href='mailto:" + encodedEmail + "'>" + encodedEmail + "</a>";
+      document.getElementById('email').innerHTML = myemail;
+      shown = true;
+    } else {
+      alert("Invalid email address!");
+    }
   }
+}
+
+function encodeInput(input) {
+  const encoded = document.createElement('div');
+  encoded.innerText = input;
+  return encoded.innerHTML;
+}
+
+function validateEmail(email) {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailPattern.test(email);
 }
