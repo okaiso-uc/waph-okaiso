181e921ee779af14b62972679b4ab67d8dc14dc7
okaiso-uc <156627165+okaiso-uc@users.noreply.github.com>
2024-02-20 21:23:38 -0500

    Update index.php

--- labs/lab3/index.php
+++ labs/lab3/index.php
@@ -1,27 +1,32 @@
 <?php
 	session_start();    
-	if (checklogin($_POST["username"],$_POST["password"])) {
+	if (checklogin($_POST["username"], $_POST["password"])) {
 ?>
 	<h2> Welcome <?php echo $_POST['username']; ?> !</h2>
 <?php		
-	}else{
+	} else {
 		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
 		die();
 	}
 
 	function checklogin($username, $password) {
-		$account = array("admin","1234");
+		$account = array("admin", "1234");
 		if ($username == $account[0] && $password == $account[1]) {
 		  return true;
 		} else {
 		  return false;
 		}
-  	}
+	}
 
 	function checklogin_mysql($username, $password) {
 		$mysqli = new mysqli('localhost', 'okaiso', '12345', 'waph');
 		if ($mysqli->connect_errno) {
 			printf("Database connection failed: %s\n", $mysqli->connect_error);
+			exit(); 
 		}
-   }
+
+		
+		return false;
+	}
 ?>
+`
