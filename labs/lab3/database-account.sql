00dcb0dc7acc642161f4d19d46fc26d4eb268422
okaiso-uc <156627165+okaiso-uc@users.noreply.github.com>
2024-02-20 20:32:54 -0500

    Update database-account.sql

--- labs/lab3/database-account.sql
+++ labs/lab3/database-account.sql
@@ -1,3 +1,3 @@
-create database waph;
-CREATE USER 'okaiso'@'localhost' IDENTIFIED BY 'Pa$$w0rd'
+CREATE DATABASE waph;
+CREATE USER 'okaiso'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
 GRANT ALL ON waph.* TO 'okaiso'@'localhost';
