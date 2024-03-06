ecb2eb86d28c80881f774068f61e4cbb6b64e08d
SethOkai <Sethokai5@gmail.com>
2024-03-06 00:20:39 -0500

    update

--- labs/lab4/index.php
+++ labs/lab4/index.php
@@ -1,18 +1,49 @@
 <?php
 	session_start();    
-	if (checklogin($_POST["username"],$_POST["password"])) {
+	if (isset($_POST["username"]) && isset($_POST["password"])) {
+		if (checklogin($_POST["username"], $_POST["password"])) {
+			$_SESSION['authenticated'] = TRUE;
+			$_SESSION['username'] = $_POST["username"];
 ?>
-	<h2> Welcome <?php echo $_POST['username']; ?> !</h2>
+
 <?php		
-	}else{
-		echo "<script>alert('Invalid username/password');window.location='form.php';</script>";
-		die();
+		} else {
+			 session_destroy();
+			 echo "<script>alert('" . htmlspecialchars('Invalid username/password', ENT_QUOTES, 'UTF-8') . "');window.location='form.php';</script>";
+			 die();
+		}
+	}
+	if(!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] != TRUE) {
+		session_destroy();
+		echo "<script>alert('you have not login. Please login first!')</script>";
+	    header("Refresh: 0; url=form.php");
+	    die();
 	}
 	function checklogin($username, $password) {
-		$account = array("admin","1234");
-		if (($username== $account[0]) and ($password == $account[1])) 
-		  return TRUE;
-		else 
-		  return FALSE;
-  	}
+		$account = array("admin", "12345");
+		if ($username == $account[0] && $password == $account[1]) {
+		  return true;
+		} else {
+		  return false;
+		}
+	}
+
+	function checklogin_mysql($username, $password) {
+		$mysqli = new mysqli('localhost', 'okaiso', '12345', 'waph');
+		if ($mysqli->connect_errno) {
+			printf("Database connection failed: %s\n", $mysqli->connect_error);
+			exit(); 
+		}
+		
+		$prepared_sql = "SELECT * FROM users where username=? AND password=md5(?);";
+		$stmt = $mysqli->prepare($prepared_sql);
+		$stmt->bind_param("ss", $username, $password);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		if($result->num_rows == 1)
+			return TRUE;
+		return FALSE;
+	}
 ?>
+		<h2> Welcome <?php echo htmlentities($_SESSION['username']) ?> !</h2>
+		<a href="logout.php">Logout</a>
---
+++ labs/lab4/logout.php
@@ -0,0 +1,7 @@
+<?php
+	session_start();
+	session_destroy();
+
+?>
+	<h2> you are logged out! </h2>
+	<a href="form.php">Login again</a>
